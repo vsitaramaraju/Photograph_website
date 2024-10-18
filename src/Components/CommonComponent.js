b99e9c3ed2f6799c75ea49dd83d3e5b4acbdf878
vsitaramaraju <vadapallisramaraju@gmail.com>
2024-10-18 19:13:53 +0530

    Code update

---
+++ src/Components/CommonComponent.js
@@ -0,0 +1,23 @@
+export const HeaderImages = [
+  "/images/image1.jpg",
+  "/images/image2.jpg",
+  "/images/image3.jpg"
+];
+
+export const Image = [
+  {
+    img1: "/images/image4.jpg",
+    img2: "/images/image5.jpg",
+    title: "Model"
+  },
+  {
+    img1: "/images/image6.jpg",
+    img2: "/images/image7.jpg",
+    title: "Marriage"
+  },
+  {
+    img1: "/images/image8.png",
+    img2: "/images/image9.jpg",
+    title: "Baby"
+  }
+];
